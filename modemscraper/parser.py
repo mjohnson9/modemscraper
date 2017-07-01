75bfc2f007af481f7e23d1196ff8d2153d245dbe
Michael Johnson <michael@johnson.computer>
2017-06-30 21:18:12 -0400

    Add copyrights & copyright compliance check

--- modemscraper/parser.py
+++ modemscraper/parser.py
@@ -1,4 +1,23 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# Copyright (C) 2017 Michael Johnson
+#
+# This file is part of modemscraper.
+#
+# modemscraper is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# modemscraper is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with modemscraper.  If not, see <http://www.gnu.org/licenses/>.
+
 """The parser module parses raw units into machine-usable values.
 
 It is a specialized module for the units found on a modem's status page. As
